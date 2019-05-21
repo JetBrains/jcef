21efb65701f65c9e1868c806b6b59778726e0bf0
Anton Tarasov <anton.tarasov@jetbrains.com>
2019-05-21 11:50:02 +0300

    Allow compilation and run with JDK11

---
+++ java/module-info.java
@@ -0,0 +1,9 @@
+module jcef {
+    exports org.cef;
+    exports org.cef.browser;
+    exports org.cef.callback;
+    exports org.cef.handler;
+    exports org.cef.misc;
+    exports org.cef.network;
+    requires java.desktop;
+}
