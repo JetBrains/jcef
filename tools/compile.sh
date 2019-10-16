3ae8062c40f6cb87fe31be75a8fac4c90efab46c
Anton Tarasov <anton.tarasov@jetbrains.com>
2019-10-16 15:04:47 +0300

    Compile on Linux with JDK11

--- tools/compile.sh
+++ tools/compile.sh
@@ -7,15 +7,19 @@ cd ..
 
 if [ -z "$1" ]; then
   echo "ERROR: Please specify a target platform: linux32 or linux64"
+elif [ -z "$2" ]; then
+  echo "ERROR: Please specify a target configuration: Release or Debug"
 else
   export OUT_PATH="./out/$1"
+  export OUT_NATIVE_PATH="./jcef_build/native/$2"
   export CLS_PATH="./third_party/jogamp/jar/*:./third_party/junit/*:./java"
 
   if [ ! -d "$OUT_PATH" ]; then
     mkdir -p "$OUT_PATH"
   fi
 
-  javac -Xdiags:verbose -cp $CLS_PATH -d $OUT_PATH java/tests/detailed/*.java java/tests/junittests/*.java java/tests/simple/*.java java/org/cef/*.java java/org/cef/browser/*.java java/org/cef/callback/*.java java/org/cef/handler/*.java java/org/cef/misc/*.java java/org/cef/network/*.java
+  #$JAVA_HOME/bin/javac -Xdiags:verbose -cp $CLS_PATH -d $OUT_PATH java/tests/detailed/*.java java/tests/junittests/*.java java/tests/simple/*.java java/org/cef/*.java java/org/cef/browser/*.java java/org/cef/callback/*.java java/org/cef/handler/*.java java/org/cef/misc/*.java java/org/cef/network/*.java
+  ant -v modular-sdk
 
   # Copy resource files.
   cp -f ./java/tests/detailed/handler/*.html $OUT_PATH/tests/detailed/handler
