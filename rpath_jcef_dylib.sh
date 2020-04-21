dd6c049b55464d73db39ee817c712613a35995bd
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-04-21 15:28:57 +0300

    [followup] JBR-2304 [jcef] add teamcity scripts to jcef repo
    - Mac part

--- rpath_jcef_dylib.sh
+++
@@ -1,4 +0,0 @@
-# Copyright 2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-install_name_tool -change @rpath/libjvm.dylib @loader_path/server/libjvm.dylib libjcef.dylib
-install_name_tool -change @rpath/libjawt.dylib @loader_path/libjawt.dylib libjcef.dylib
