ded058e85cdd1085b31a922ed86a3faf44ac5a2a
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-05-28 14:22:54 +0300

    [followup] JBR-2439 [jcef] mouse listener not called on second hidpi monitor

--- java/org/cef/browser/mac/CefBrowserWindowMac.java
+++ java/org/cef/browser/mac/CefBrowserWindowMac.java
@@ -9,6 +9,10 @@ import java.awt.peer.ComponentPeer;
 
 import com.jetbrains.cef.JdkEx;
 import sun.awt.AWTAccessor;
+import sun.lwawt.LWComponentPeer;
+import sun.lwawt.PlatformWindow;
+import sun.lwawt.macosx.CFRetainedResource;
+import sun.lwawt.macosx.CPlatformWindow;
 
 import org.cef.browser.CefBrowserWindow;
 
