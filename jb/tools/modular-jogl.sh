ca8c3454613e5a276a72cbbe40ad46d336ec90d7
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-09-23 16:42:00 +0300

    Add exports to module-info.java for jogl

--- jb/tools/modular-jogl.sh
+++ jb/tools/modular-jogl.sh
@@ -17,6 +17,22 @@ mkdir jogl.all
 echo "module jogl.all {
   requires java.desktop;
   requires gluegen.rt;
+
+  // to jcef
+  exports com.jogamp.opengl.awt;
+  exports com.jogamp.nativewindow;
+  exports com.jogamp.opengl;
+
+  exports jogamp.nativewindow.x11 to gluegen.rt;
+  exports jogamp.opengl.x11.glx to gluegen.rt;
+  exports jogamp.opengl.gl4 to gluegen.rt;
+  exports jogamp.opengl.egl to gluegen.rt;
+  exports jogamp.nativewindow.x11.awt to gluegen.rt;
+  exports jogamp.opengl.awt to gluegen.rt;
+
+  opens jogamp.opengl.x11.glx to gluegen.rt;
+  opens com.jogamp.opengl.egl to gluegen.rt;
+  opens jogamp.opengl.gl4 to gluegen.rt;
 }" > jogl.all/module-info.java
 
 "$JAVA_HOME"/bin/javac --module-path gluegen-rt.jar --patch-module jogl.all=jogl-all.jar jogl.all/module-info.java
