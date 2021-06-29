cb0cf046ad71e9aba0ee5f4b03f9a69b606b156b
Anton Tarasov <anton.tarasov@jetbrains.com>
2021-06-29 17:00:31 +0300

    JBR-3426: [followup] add checks
    (cherry picked from commit 41730422f9d927f42feee06263956f5c3df66cf5)

--- cmake/PatchCEF.cmake
+++ cmake/PatchCEF.cmake
@@ -8,6 +8,11 @@ function(PatchCEF platform version)
   set(CEF_PATCH_IN "${CEF_PATCH_DIR}/${CEF_PATCH_NAME}.in")
   set(CEF_PATCH "${CEF_PATCH_DIR}/${CEF_PATCH_NAME}.patch")
 
+  find_program(SED sed)
+  if(NOT SED)
+    message(FATAL_ERROR "No `sed` tool found")
+  endif()
+
   execute_process(
     COMMAND "sed"
             "s/CEF_BINARY_ROOT/${CEF_DISTRIBUTION}/g;w ${CEF_PATCH}"
@@ -16,6 +21,10 @@ function(PatchCEF platform version)
     RESULT_VARIABLE git_patch
     OUTPUT_QUIET
   )
+
+  if(NOT git_patch STREQUAL "0")
+    message(FATAL_ERROR "Can't apply 'sed' on the patch, result: ${git_patch}")
+  endif()
 
   execute_process(
     COMMAND "git"
@@ -32,6 +41,10 @@ function(PatchCEF platform version)
     message(WARNING "Can't apply patches to CEF includes, result: ${git_apply_result}")
   endif()
 
+  if(NOT EXISTS ${PROJECT_SOURCE_DIR}/jb_cef_binary)
+    message(FATAL_ERROR "No 'jb_cef_binary' dir with the patched cef found")
+  endif()
+
   # replace original libcef binary (and dependent) with patched binaries
   if("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
     execute_process(
@@ -42,7 +55,7 @@ function(PatchCEF platform version)
         RESULT_VARIABLE remove_result
       )
     if(NOT remove_result STREQUAL "0")
-      message(WARNING "Can't erase origin libcef, result: ${remove_result}")
+      message(FATAL_ERROR "Can't erase origin libcef, result: ${remove_result}")
     endif()
 
     execute_process(
@@ -81,6 +94,6 @@ function(PatchCEF platform version)
   endif()
 
   if(NOT replace_result STREQUAL "0")
-    message(WARNING "Can't replace patched libcef, result: ${replace_result}")
+    message(FATAL_ERROR "Can't replace patched libcef, result: ${replace_result}")
   endif()
 endfunction()
