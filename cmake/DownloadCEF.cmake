8be3c18cfeab739f6b7fca53ea19ccfa87047d63
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2021-09-08 19:16:56 +0300

    Remove PatchCEF.cmake
    use binary distrib from TC (or aws s3)

--- cmake/DownloadCEF.cmake
+++ cmake/DownloadCEF.cmake
@@ -10,7 +10,7 @@
 
 function(DownloadCEF platform version download_dir)
   # Specify the binary distribution type and download directory.
-  set(CEF_DISTRIBUTION "cef_binary_${version}_${platform}")
+  set(CEF_DISTRIBUTION "cef_binary_${version}_${platform}_minimal")
   set(CEF_DOWNLOAD_DIR "${download_dir}")
 
   # The location where we expect the extracted binary distribution.
@@ -18,30 +18,32 @@ function(DownloadCEF platform version download_dir)
 
   # Download and/or extract the binary distribution if necessary.
   if(NOT IS_DIRECTORY "${CEF_ROOT}")
-    set(CEF_DOWNLOAD_FILENAME "${CEF_DISTRIBUTION}.tar.bz2")
+    set(CEF_DOWNLOAD_FILENAME "${CEF_DISTRIBUTION}.zip")
     set(CEF_DOWNLOAD_PATH "${CEF_DOWNLOAD_DIR}/${CEF_DOWNLOAD_FILENAME}")
     if(NOT EXISTS "${CEF_DOWNLOAD_PATH}")
-      set(CEF_DOWNLOAD_URL "https://cef-builds.spotifycdn.com/${CEF_DOWNLOAD_FILENAME}")
+      set(CEF_DOWNLOAD_URL "https://cache-redirector.jetbrains.com/intellij-jbr/${CEF_DOWNLOAD_FILENAME}")
       string(REPLACE "+" "%2B" CEF_DOWNLOAD_URL_ESCAPED ${CEF_DOWNLOAD_URL})
-
-      # Download the SHA1 hash for the binary distribution.
-      message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}.sha1 from ${CEF_DOWNLOAD_URL_ESCAPED}...")
-      file(DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}.sha1" "${CEF_DOWNLOAD_PATH}.sha1")
-      file(READ "${CEF_DOWNLOAD_PATH}.sha1" CEF_SHA1)
+      # Download the sha256sum hash for the binary distribution.
+      message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}.checksum from ${CEF_DOWNLOAD_URL_ESCAPED}...")
+      file(DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}.checksum" "${CEF_DOWNLOAD_PATH}.checksum")
+      file(READ "${CEF_DOWNLOAD_PATH}.checksum" CEF_SHA256_RAW)
+      string(REGEX MATCH "[0-9a-fA-F]+" CEF_SHA256 "${CEF_SHA256_RAW}")
 
       # Download the binary distribution and verify the hash.
       message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}...")
       file(
         DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}" "${CEF_DOWNLOAD_PATH}"
-        EXPECTED_HASH SHA1=${CEF_SHA1}
+        EXPECTED_HASH SHA256=${CEF_SHA256}
         SHOW_PROGRESS
         )
+    else()
+      message(STATUS "Extracting downloaded artifact ${CEF_DOWNLOAD_PATH}...")
     endif()
 
     # Extract the binary distribution.
     message(STATUS "Extracting ${CEF_DOWNLOAD_PATH}...")
     execute_process(
-      COMMAND ${CMAKE_COMMAND} -E tar xzf "${CEF_DOWNLOAD_DIR}/${CEF_DOWNLOAD_FILENAME}"
+      COMMAND ${CMAKE_COMMAND} -E tar xzf "${CEF_DOWNLOAD_PATH}"
       WORKING_DIRECTORY ${CEF_DOWNLOAD_DIR}
       )
   endif()
