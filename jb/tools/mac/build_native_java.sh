f13c44610ab52bdcb4785aebeff7907cf3c3d4b1
Anton Tarasov <anton.tarasov@jetbrains.com>
2022-04-22 18:53:08 +0300

    JBR-4368 [dev branch] Set up JCEF builds for Linux aarch64/arm64

--- jb/tools/mac/build_native_java.sh
+++ jb/tools/mac/build_native_java.sh
@@ -1,49 +1,41 @@
-# Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+#!/bin/bash
+# Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+set -euo pipefail
 
-source set_env.sh || exit 1
+script_dir=$(cd -- "$(dirname -- "$0")" &>/dev/null && pwd)
 
-# shellcheck source=../common/common.sh
-source "$JB_TOOLS_DIR"/common/common.sh || exit 1
+source "$script_dir/set_env.sh"
 
-OUT_DIR=$JCEF_ROOT_DIR/jcef_build
-OUT_JAVA_DIR=$JCEF_ROOT_DIR/out
+OUT_DIR="$JCEF_ROOT_DIR/jcef_build"
+OUT_JAVA_DIR="$JCEF_ROOT_DIR/out"
 
-if [ "$1" == "clean" ]; then
-    echo "*** delete $OUT_DIR..."
-    rm -rf "$OUT_DIR"
-    echo "*** delete $OUT_JAVA_DIR..."
-    rm -rf "$OUT_JAVA_DIR"
-    exit 0
+if [ "${1:-}" == "clean" ]; then
+  echo "*** delete $OUT_DIR..."
+  rm -rf "$OUT_DIR"
+  echo "*** delete $OUT_JAVA_DIR..."
+  rm -rf "$OUT_JAVA_DIR"
+  exit 0
 fi
-mkdir -p "$OUT_DIR" || do_fail
-mkdir -p "$OUT_JAVA_DIR" || do_fail
-
-case "$2" in
-    "arm64")
-        export TARGET_ARCH="arm64"
-        ;;
-    "x86_64")
-        export TARGET_ARCH="x86_64"
-        ;;
-esac
+mkdir -p "$OUT_DIR"
+mkdir -p "$OUT_JAVA_DIR"
 
 echo "*** run cmake [TARGET=$TARGET_ARCH]..."
-cd "$OUT_DIR" || do_fail
-cmake -G "Xcode" -DPROJECT_ARCH="$TARGET_ARCH" .. || do_fail
+cd "$OUT_DIR" || exit 1
+cmake -G "Xcode" -DPROJECT_ARCH="$TARGET_ARCH" ..
 
 echo "*** run xcodebuild..."
-xcodebuild -configuration Release || do_fail
+xcodebuild -configuration Release
 
 echo "*** change @rpath in libjcef.dylib..."
-cd "$JCEF_ROOT_DIR"/jcef_build/native/Release || do_fail
-install_name_tool -change @rpath/libjvm.dylib @loader_path/server/libjvm.dylib libjcef.dylib || do_fail
-install_name_tool -change @rpath/libjawt.dylib @loader_path/libjawt.dylib libjcef.dylib || do_fail
+cd "$OUT_DIR"/native/Release || exit 1
+install_name_tool -change @rpath/libjvm.dylib @loader_path/server/libjvm.dylib libjcef.dylib
+install_name_tool -change @rpath/libjawt.dylib @loader_path/libjawt.dylib libjcef.dylib
 
 if [ "$TARGET_ARCH" == "arm64" ]; then
-    JNF_RPATH=$(otool -L libjcef.dylib | grep JavaNativeFoundation | awk '{print $1}')
-    test -z "$JNF_RPATH" || install_name_tool -change "$JNF_RPATH" @loader_path/../../Frameworks/JavaNativeFoundation.framework/JavaNativeFoundation libjcef.dylib || do_fail
+  if otool -L libjcef.dylib | grep -q JavaNativeFoundation; then
+    JNF_RPATH="$(otool -L libjcef.dylib | grep JavaNativeFoundation | awk '{print $1}')"
+    install_name_tool -change "$JNF_RPATH" @loader_path/../../Frameworks/JavaNativeFoundation.framework/JavaNativeFoundation libjcef.dylib
+  fi
 fi
 
-cp libjcef.dylib jcef_app.app/Contents/Java || do_fail
-
-cd "$JB_TOOLS_OS_DIR" || do_fail
+cp libjcef.dylib jcef_app.app/Contents/Java
--- jb/tools/mac/build_universal.sh
+++ jb/tools/mac/build_universal.sh
@@ -1,9 +1,10 @@
-# Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+#!/bin/bash
+# Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+set -euo pipefail
 
-if ! source set_env.sh
-then
-    exit 1
-fi
+script_dir=$(cd -- "$(dirname -- "$0")" &>/dev/null && pwd)
+
+source "$script_dir/set_env.sh"
 
 function test_arm64 {
   __file=$*
