9ea01c581ee14ac0def2979303f897922760a76d
Leonid Shalupov <leonid@shalupov.com>
2022-05-07 16:11:44 +0200

    JBR-4476 move TC build steps to scripts and Dockerfile (xenial-based)
    ref JBR-4484

---
+++ jb/tools/linux/teamcity_build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -exuo pipefail
+
+# CD to script's directory
+cd "$(dirname "$0")"
+
+case $(uname -m) in
+  x86_64)
+    arch=x86_64
+    cef_platform=64
+    ;;
+  aarch64)
+    arch=arm64
+    cef_platform=arm64
+    ;;
+  *)
+    echo "Unsupported arch: $(uname -m)"
+    exit 1
+    ;;
+esac
+
+cef_dir=../../../third_party/cef
+
+shopt -s nullglob
+cef_distributions=($cef_dir/cef_binary_*_linux${cef_platform}_minimal.zip)
+echo ${#cef_distributions[@]}
+if [ ${#cef_distributions[@]} != 1 ]; then
+  echo "ERROR Expected one and only one CEF distribution file at $cef_dir" >&2
+  ls -la "$cef_dir"
+  exit 1
+fi
+cef_dist=$(basename ${cef_distributions[0]})
+echo "Matched CEF distribution $cef_dist"
+
+[[ $cef_dist =~ cef_binary_(.*)_linux${cef_platform}_minimal\.zip ]]
+export CEF_VERSION="${BASH_REMATCH[1]}"
+
+echo "CEF version is $CEF_VERSION"
+
+# CEF is already downloaded, do not download another one in any case
+export CEF_DONT_DOWNLOAD=true
+
+echo "*** build all..."
+bash build.sh all "$arch"
