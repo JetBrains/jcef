c0cc6cfd46203e04e09eaf6e5b9a60d5f7193e88
Leonid Shalupov <leonid@shalupov.com>
2022-05-14 21:02:51 +0200

    build: use JDK11 from Docker image

--- jb/tools/linux/teamcity_build.sh
+++ jb/tools/linux/teamcity_build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -exuo pipefail
 
-echo "*** download openjdk-11..."
-wget --no-check-certificate -c https://corretto.aws/downloads/latest/amazon-corretto-11-$(uname -m)-linux-jdk.tar.gz -O openjdk.tar.gz > /dev/null 2>&1
-rm -rf jdk11
-mkdir jdk11
-tar xzf openjdk.tar.gz --strip-components 1 -C jdk11
-export JDK_11="$(pwd)/jdk11"
+# installed in Dockerfile
+export JDK_11=/usr/lib/jvm/java-11-openjdk-amd64
 "$JDK_11/bin/java" -version
 
 echo "*** build all..."
