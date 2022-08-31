fc9ec30795a4db27a7952da53a982f55f47d2e1c
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2022-08-31 11:47:15 +0700

    [scripts] download cef binary from cef-builds.spotifycdn if necessary

--- cmake/DownloadCEF.cmake
+++ cmake/DownloadCEF.cmake
@@ -16,26 +16,43 @@ function(DownloadCEF platform version download_dir)
   # The location where we expect the extracted binary distribution.
   set(CEF_ROOT "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}" CACHE INTERNAL "CEF_ROOT")
 
+    #cef_binary_104.4.26+g4180781+chromium-104.0.5112.102_macosarm64_minimal.tar.bz2
   # Download and/or extract the binary distribution if necessary.
   if(NOT IS_DIRECTORY "${CEF_ROOT}")
-    set(CEF_DOWNLOAD_FILENAME "${CEF_DISTRIBUTION}.zip")
-    set(CEF_DOWNLOAD_PATH "${CEF_DOWNLOAD_DIR}/${CEF_DOWNLOAD_FILENAME}")
-    if(NOT EXISTS "${CEF_DOWNLOAD_PATH}")
-      set(CEF_DOWNLOAD_URL "https://cache-redirector.jetbrains.com/intellij-jbr/${CEF_DOWNLOAD_FILENAME}")
+    if(NOT EXISTS "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.zip" AND
+       NOT EXISTS "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.tar.bz2")
+      set(CEF_DOWNLOAD_URL "https://cache-redirector.jetbrains.com/intellij-jbr/${CEF_DISTRIBUTION}.zip")
       string(REPLACE "+" "%2B" CEF_DOWNLOAD_URL_ESCAPED ${CEF_DOWNLOAD_URL})
       # Download the sha256sum hash for the binary distribution.
-      message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}.checksum from ${CEF_DOWNLOAD_URL_ESCAPED}...")
-      file(DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}.checksum" "${CEF_DOWNLOAD_PATH}.checksum")
-      file(READ "${CEF_DOWNLOAD_PATH}.checksum" CEF_SHA256_RAW)
-      string(REGEX MATCH "[0-9a-fA-F]+" CEF_SHA256 "${CEF_SHA256_RAW}")
-
-      # Download the binary distribution and verify the hash.
-      message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}...")
-      file(
-        DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}" "${CEF_DOWNLOAD_PATH}"
-        EXPECTED_HASH SHA256=${CEF_SHA256}
-        SHOW_PROGRESS
-        )
+      message(STATUS "Downloading ${CEF_DISTRIBUTION}.zip.checksum from ${CEF_DOWNLOAD_URL_ESCAPED}...")
+      file(DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}.checksum" "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.zip.checksum")
+      file (SIZE "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.zip.checksum" CHECKSUM_FILE_SIZE)
+      if(NOT "${CHECKSUM_FILE_SIZE}" STREQUAL "0")
+          file(READ "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.zip.checksum" CEF_SHA256_RAW)
+          string(REGEX MATCH "[0-9a-fA-F]+" CEF_SHA256 "${CEF_SHA256_RAW}")
+          # Download the binary distribution and verify the hash.
+          message(STATUS "Downloading intellij ${CEF_DISTRIBUTION}.zip...")
+          set(CEF_DOWNLOAD_PATH ${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.zip)
+          file(
+            DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}" "${CEF_DOWNLOAD_PATH}"
+            EXPECTED_HASH SHA256=${CEF_SHA256}
+            SHOW_PROGRESS
+            )
+      else()
+          set(CEF_DOWNLOAD_URL "https://cef-builds.spotifycdn.com/${CEF_DISTRIBUTION}.tar.bz2")
+          string(REPLACE "+" "%2B" CEF_DOWNLOAD_URL_ESCAPED ${CEF_DOWNLOAD_URL})
+          # Download the SHA1 hash for the binary distribution.
+          message(STATUS "Downloading ${CEF_DISTRIBUTION}.tar.bz2.sha1 from ${CEF_DOWNLOAD_URL_ESCAPED}...")
+          file(DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}.sha1" "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.tar.bz2.sha1")
+          file(READ "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.tar.bz2.sha1" CEF_SHA1)
+          message(STATUS "Downloading cef-builds.spotifycdn ${CEF_DISTRIBUTION}.tar.bz2...")
+          set(CEF_DOWNLOAD_PATH ${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}.tar.bz2)
+          file(
+            DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}" "${CEF_DOWNLOAD_PATH}"
+            EXPECTED_HASH SHA1=${CEF_SHA1}
+            SHOW_PROGRESS
+            )
+      endif()
     else()
       message(STATUS "Extracting downloaded artifact ${CEF_DOWNLOAD_PATH}...")
     endif()
--- jb/tools/linux/teamcity_build.sh
+++ jb/tools/linux/teamcity_build.sh
@@ -19,26 +19,30 @@ case $(uname -m) in
     ;;
 esac
 
-cef_dir=../../../third_party/cef
+if [ -n "${CEF_USE_LOCAL:-}" ]; then
+  cef_dir=../../../third_party/cef
 
-shopt -s nullglob
-cef_distributions=($cef_dir/cef_binary_*_linux${cef_platform}_minimal.zip)
-echo ${#cef_distributions[@]}
-if [ ${#cef_distributions[@]} != 1 ]; then
-  echo "ERROR Expected one and only one CEF distribution file at $cef_dir" >&2
-  ls -la "$cef_dir"
-  exit 1
+  shopt -s nullglob
+  cef_distributions=($cef_dir/cef_binary_*_linux${cef_platform}_minimal.zip)
+  echo ${#cef_distributions[@]}
+  if [ ${#cef_distributions[@]} != 1 ]; then
+    echo "ERROR Expected one and only one CEF distribution file at $cef_dir" >&2
+    ls -la "$cef_dir"
+    exit 1
+  fi
+  cef_dist=$(basename ${cef_distributions[0]})
+  echo "Matched CEF distribution $cef_dist"
+
+  [[ $cef_dist =~ cef_binary_(.*)_linux${cef_platform}_minimal\.zip ]]
+  export CEF_VERSION="${BASH_REMATCH[1]}"
+
+  echo "CEF version is $CEF_VERSION"
+
+  # CEF is already downloaded, do not download another one in any case
+  export CEF_DONT_DOWNLOAD=true
+else
+  echo "CEF distribution will be downloaded during building"
 fi
-cef_dist=$(basename ${cef_distributions[0]})
-echo "Matched CEF distribution $cef_dist"
-
-[[ $cef_dist =~ cef_binary_(.*)_linux${cef_platform}_minimal\.zip ]]
-export CEF_VERSION="${BASH_REMATCH[1]}"
-
-echo "CEF version is $CEF_VERSION"
-
-# CEF is already downloaded, do not download another one in any case
-export CEF_DONT_DOWNLOAD=true
 
 echo "*** build all..."
 bash build.sh all "$arch"
