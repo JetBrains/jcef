1390b40052b3f86d12f02ba9cb695cf3290629f8
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2023-09-01 19:30:01 +0200

    add submodule fix path

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -1,12 +1,11 @@
-#set(JCEF_VCPKG_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/third_party/vcpk)
 #set(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake"
 #        CACHE STRING "Vcpkg toolchain file")
 
-function(bring_vcpkg vcpkg_path)
+set(JCEF_VCPKG_DIRECTORY ${CMAKE_SOURCE_DIR}/third_party/vcpkg)
+function(bring_vcpkg)
     find_package(Git REQUIRED)
     execute_process(
-            execute_process (COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+            COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
             COMMAND_ERROR_IS_FATAL ANY)
-    )
 endfunction()
