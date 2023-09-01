719d84b6fe6499778b22545c3ef5bd381719b588
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2023-09-01 19:30:01 +0200

    add submodule fix path

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -1,12 +1,22 @@
-#set(JCEF_VCPKG_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/third_party/vcpk)
 #set(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake"
 #        CACHE STRING "Vcpkg toolchain file")
 
-function(bring_vcpkg vcpkg_path)
+set(JCEF_VCPKG_DIRECTORY ${CMAKE_SOURCE_DIR}/third_party/vcpkg)
+function(bring_vcpkg)
     find_package(Git REQUIRED)
     execute_process(
-            execute_process (COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-            COMMAND_ERROR_IS_FATAL ANY)
-    )
+            COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            COMMAND_error_is_fatal ANY)
+    if(OS_WINDOWS)
+        execute_process(
+                COMMAND cmd /C ${JCEF_VCPKG_DIRECTORY}/bootstrap-vcpkg.bat -disableMetrics
+                WORKING_DIRECTORY ${JCEF_VCPKG_DIRECTORY}
+                COMMAND_ERROR_IS_FATAL ANY)
+    else ()
+        execute_process(
+                COMMAND bash ${JCEF_VCPKG_DIRECTORY}/bootstrap-vcpkg.sh -disableMetrics
+                WORKING_DIRECTORY ${JCEF_VCPKG_DIRECTORY}
+                COMMAND_ERROR_IS_FATAL ANY)
+    endif()
 endfunction()
