dba8b5424016ee4749808acb67f3ef6683ee5cae
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2024-02-14 13:46:28 +0400

    [tests] Add extra logging

--- java_tests/tests/junittests/TestFrame.java
+++ java_tests/tests/junittests/TestFrame.java
@@ -230,7 +230,9 @@ class TestFrame extends JFrame implements CefLifeSpanHandler, CefLoadHandler, Ce
 
     @Override
     public void onLoadingStateChange(
-            CefBrowser browser, boolean isLoading, boolean canGoBack, boolean canGoForward) {}
+            CefBrowser browser, boolean isLoading, boolean canGoBack, boolean canGoForward) {
+        if (debugPrint()) CefLog.Info("onLoadingStateChange id=%d, isLoading=%s", browser.getIdentifier(), String.valueOf(isLoading));
+    }
 
     @Override
     public void onLoadStart(CefBrowser browser, CefFrame frame, TransitionType transitionType) {}
