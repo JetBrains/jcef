291728c815c76ed14f57132c83d9ed7e3f1997c3
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2024-02-21 14:57:13 +0400

    fix basic test

--- java_tests/tests/basic/BasicJcefTest.java
+++ java_tests/tests/basic/BasicJcefTest.java
@@ -171,6 +171,9 @@ public class BasicJcefTest {
         if (frame[0] != null)
             frame[0].dispose();
 
+        // dispose CefApp
+        TestSetupExtension.shutdonwCef();
+
         CefLog.Info("Basic checks spent %d ms", System.currentTimeMillis() - time0);
     }
 
