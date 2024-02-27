7d817ad3e5f83c74c01cfbc35a4e0fa5431f59f5
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2024-02-27 08:48:35 +0400

    JBR-4430 Fix thrift exports

--- jb/tools/common/thrift-module-info.java
+++ jb/tools/common/thrift-module-info.java
@@ -1,4 +1,8 @@
 module org.apache.thrift {
 	requires java.base;
 	exports org.apache.thrift;
+	exports org.apache.thrift.server;
+	exports org.apache.thrift.protocol;
+	exports org.apache.thrift.scheme;
+	exports org.apache.thrift.meta_data;
 }
