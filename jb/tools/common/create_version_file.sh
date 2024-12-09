5e2446a908b65cac479d0640bea36e5d8ddb13a0
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2024-12-09 14:05:47 +0400

    fixup! [remote] add sha to version

--- jb/tools/common/create_version_file.sh
+++ jb/tools/common/create_version_file.sh
@@ -6,3 +6,5 @@ fi
 echo "*** create jcef.version..."
 grep "#define JCEF_VERSION" "$JCEF_ROOT_DIR"/native/jcef_version.h | sed 's/#define JCEF_VERSION /JCEF_VERSION=/g' > "$1/jcef.version"
 grep "#define JCEF_COMMIT_HASH" "$JCEF_ROOT_DIR"/native/jcef_version.h | sed 's/#define JCEF_COMMIT_HASH /JCEF_COMMIT_HASH=/g' | tee -a "$1/jcef.version"
+echo "jcef.version:"
+cat "$1/jcef.version"
