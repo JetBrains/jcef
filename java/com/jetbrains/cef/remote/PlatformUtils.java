5d8a706f7aa2080db90e648ae9d95a71d7c2862e
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2025-05-22 12:31:42 +0400

    [remote] Minor fix for shared_mem_helper loading

--- java/com/jetbrains/cef/remote/PlatformUtils.java
+++ java/com/jetbrains/cef/remote/PlatformUtils.java
@@ -1,12 +1,13 @@
 package com.jetbrains.cef.remote;
 
+import com.jetbrains.cef.SharedMemory;
 import com.jetbrains.cef.remote.thrift_codegen.CefKeyEventAttributes;
 
 import java.awt.event.KeyEvent;
 
 public class PlatformUtils {
     static {
-        System.loadLibrary("shared_mem_helper");
+        SharedMemory.loadDynamicLib();
     }
 
     public static native CefKeyEventAttributes getCefKeyEventAttributes(KeyEvent e);
