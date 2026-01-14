912e5d3bc5c12694674cfd8b681e90a35fb848d2
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2026-01-14 18:59:54 +0100

    JBR-9801 build: windows: add version.info to native bundle

--- jb/tools/windows/create_bundle.bat
+++ jb/tools/windows/create_bundle.bat
@@ -49,6 +49,9 @@ if exist "%JCEF_ROOT_DIR%\%ARTIFACT_SERVER%.tar.gz" (
 bash -c "tar -cvzf $ARTIFACT_SERVER.tar.gz -C cef_server $(ls cef_server)" || goto:__exit
 rmdir /s /q cef_server || goto:__exit
 
+sed -i "s/\r$//" "%JB_TOOLS_DIR%"\common\create_version_file.sh
+bash "%JB_TOOLS_DIR%"\common\create_version_file.sh %ARTIFACT_DIR%
+
 echo *** create native standalone bundle...
 if exist native_bundle rd /s /q native_bundle
 mkdir native_bundle
@@ -58,11 +61,9 @@ copy /Y "%OUT_REMOTE_DIR%\shared_mem_helper.dll" native_bundle\
 copy /Y "%OUT_NATIVE_DIR%\jcef.dll" native_bundle\
 copy /Y "%OUT_NATIVE_DIR%\jcef_helper.dll" native_bundle\
 copy /Y "%OUT_NATIVE_DIR%\jcef_helper.exe" native_bundle\
+copy /Y "%ARTIFACT_DIR%\jcef.version" native_bundle\
 del /f /q native_bundle\*.pdb
 del /f /q native_bundle\*.log
-
-sed -i "s/\r$//" "%JB_TOOLS_DIR%"\common\create_version_file.sh
-bash "%JB_TOOLS_DIR%"\common\create_version_file.sh %ARTIFACT_DIR%
 
 bash -c "tar -cvzf $ARTIFACT_DIR.tar.gz -C $ARTIFACT_DIR $(ls $ARTIFACT_DIR)" || goto:__exit
 rmdir /s /q %ARTIFACT_DIR% || goto:__exit
